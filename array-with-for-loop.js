234531baf8f379ad44a1a62035cfb21564de54d9
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-01-31 16:42:38 +0600

    Array with for loop

---
+++ array-with-for-loop.js
@@ -0,0 +1,17 @@
+// Problem Name: write a code and print value of array all index using loop
+
+// Some String in Array
+var mySkill = ['Html', 'CSS', 'JavaScript', 'C++', 'Python', 'MongoDB'];
+
+// loop star with 0 and finish 1 less than the array length
+for(var num=0; num<mySkill.length; num++){
+    console.log("Index number " + num + ": " + mySkill[num]);
+}
+
+// Problem Solved and some other information:
+// -----------------------------------------------------
+/*
+    Coder: Md Mahfuz RP
+    Problem owner: Programming Hero Web Development Course
+*/
+// -----------------------------------------------------
