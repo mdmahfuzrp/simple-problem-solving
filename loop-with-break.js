8f975bc70b6bb85a301baa82ee3408d26ce26d8d
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-01-31 16:52:47 +0600

    Loop with break

---
+++ loop-with-break.js
@@ -0,0 +1,17 @@
+// Problem Name: write a code and print number between 30 to 86 and break on 44
+
+// loop star with 30 and break whole loop on 44
+for(var i = 30; i<=86; i++){
+    if(i==44){
+        break;
+    }
+    console.log(i);
+}
+
+// Problem Solved and some other information:
+// -----------------------------------------------------
+/*
+    Coder: Md Mahfuz RP
+    Problem owner: Programming Hero Web Development Course
+*/
+// -----------------------------------------------------
