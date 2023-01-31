f06a665007b8826a36596f9d5dd40048ae575a20
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-01-31 16:20:30 +0600

    Even Odd number using loop

---
+++ even-odd-number.js
@@ -0,0 +1,25 @@
+// Problem Name:
+    // write a code and print even numbers between 412 to 456 using for loop
+    // write a code and print odd numbers between 581 to 623 using while loop
+
+// even number using for loop
+console.log("The Even Number Between 412 to 456:");
+for(var firstNumber = 412; firstNumber <=456; firstNumber+=2){
+    console.log(firstNumber);
+}
+
+// odd number using while loop
+console.log("The Odd Number Between 581 to 623:");
+var startingNumber = 581;
+while(startingNumber <= 623){
+    console.log(startingNumber);
+    startingNumber+=2;
+}
+
+// Problem Solved and some other information:
+// -----------------------------------------------------
+/*
+    Coder: Md Mahfuz RP
+    Problem owner: Programming Hero Web Development Course
+*/
+// -----------------------------------------------------
