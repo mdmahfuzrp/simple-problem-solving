0a27a59fc4ebaf609bd4ae03773d426429729985
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-01-31 16:47:48 +0600

    Array with while loop

---
+++ array-with-while-loop.js
@@ -0,0 +1,19 @@
+// Problem Name: write a code and print value of array all index using while loop
+
+// Some String in Array
+var mobile = ['Iphone', 'Walton', 'Techno', 'Nokia', 'Oppo', 'Realme'];
+
+// loop star with 0 and finish 1 less than the array length
+var mobileIndex = 0;
+while(mobileIndex<mobile.length){
+    console.log("Index number " + mobileIndex + ": " + mobile[mobileIndex]);
+    mobileIndex++;
+}
+
+// Problem Solved and some other information:
+// -----------------------------------------------------
+/*
+    Coder: Md Mahfuz RP
+    Problem owner: Programming Hero Web Development Course
+*/
+// -----------------------------------------------------
