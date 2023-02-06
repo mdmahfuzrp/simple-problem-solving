017e41265afc8237874c7ecc5043ebd20823e31d
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-02-06 16:16:36 +0600

    Celsius to Fahrenheit converter

---
+++ celsius-to-fahrenheit.js
@@ -0,0 +1,18 @@
+// Celsius to Fahrenheit
+function celsToFah(celsius){
+
+    // Formula for Celsius to Fahrenheit Convertion
+    var fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
+
+const fahrenheitResult = celsToFah(20);
+console.log(fahrenheitResult);
+
+// Problem Solved and some other information:
+// -----------------------------------------------------
+/*
+    Coder: Md Mahfuz RP
+    Problem owner: Programming Hero Web Development Course
+*/
+// -----------------------------------------------------
---
+++ find-leap-year-from-array.js
@@ -0,0 +1,7 @@
+var students = {
+    name: 'mahfuz',
+    roll: 190001,
+    dept: 'computer'
+}
+
+console.log(students.dept);
