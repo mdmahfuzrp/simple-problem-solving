2dfad419362b0f92b3e92020796d5ac1e8917072
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-02-06 16:26:49 +0600

    Inches to feet converter

---
+++ inch-to-feet.js
@@ -0,0 +1,21 @@
+// Problem Name: get the inch input and calculate inch to feet
+function inchToFeet(inch){
+    var feet = inch / 12;
+    // return the result of feet to inch
+    return feet;
+}
+// Input or inch
+const inch = 40;
+// Recived the result from function
+const getFeet = parseFloat(inchToFeet(inch).toFixed(2));
+// Output
+console.log(getFeet);
+
+
+// Problem Solved and some other information:
+// -----------------------------------------------------
+/*
+    Coder: Md Mahfuz RP
+    Problem owner: Programming Hero Web Development Course
+*/
+// -----------------------------------------------------
